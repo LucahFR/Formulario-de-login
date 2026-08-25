d19acb283aedcc687d115d0af7e77d981bbe2f51
Lucas Ruhoff <ruhofflucas@gmail.com>
2026-08-25 16:59:52 -0300

    feat: funções/usuarios + style

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+
+//usuários cadastrados
+const usuarios = [
+    { username: "user1", password: "1" },
+    { username: "user2", password: "2" },
+    { username: "user3", password: "3" }
+];
+
 //Elementos
+const form = document.querySelector("form");
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
 
 //Funções
 
