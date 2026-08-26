d25d17c0ea9d57bd2d106663cd24bf270c790b1a
Lucas Ruhoff <ruhofflucas@gmail.com>
2026-08-26 16:32:31 -0300

    feat: index fix + functions validations + profile html created

--- script.js
+++ script.js
@@ -12,22 +12,40 @@ const usernameInput = document.getElementById("username");
 const passwordInput = document.getElementById("password");
 
 //Funções
+function redirectToProfile() {
+    window.location.href = "perfil.html";
+}
+
+function loginValido(username, password) {
+    return usuarios.some(usuario => usuario.username === username && usuario.password === password);
+}
 
 function login(){
     // depois de fazer login checar se tem informações dentro do sistema, caso não, mandar para tela de informações, se sim mandar para perfil
-    
+    if (usernameInput.value === "" || passwordInput.value === "") {
+        alert("Por favor, preencha todos os campos.");
+        return;
+    }
+    if (!loginValido(usernameInput.value, passwordInput.value)) {
+        redirectToProfile();
+    }
 }
 
 function registrar(){
     // Após registro mandar para adicionar informações, depois para perfil
+    if (usernameInput.value === "" || passwordInput.value === "") {
+        alert("Por favor, preencha todos os campos.");
+        return;
+    }
+
 }
 
 function adicionarInformações(){
-
+    redirectToProfile();
 }
 
 function editarInformações(){
-
+    // Após editar informações mandar para perfil, caso quer editar novamente, mandar para tela de adicionarinformações
 }
 //DOMcontentLoaded + Eventos
 
