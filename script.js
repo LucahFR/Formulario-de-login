8633ad35a0e64edb749decc2323b8e52b607dd61
Lucas Ruhoff <ruhofflucas@gmail.com>
2026-08-26 16:00:40 -0300

    feat: functions + DOMcontetLoaded + events

--- script.js
+++ script.js
@@ -7,10 +7,31 @@ const usuarios = [
 ];
 
 //Elementos
-const form = document.querySelector("form");
+const formulario = document.querySelector("formulario");
 const usernameInput = document.getElementById("username");
 const passwordInput = document.getElementById("password");
 
 //Funções
 
+function login(){
+    // depois de fazer login checar se tem informações dentro do sistema, caso não, mandar para tela de informações, se sim mandar para perfil
+    
+}
+
+function registrar(){
+    // Após registro mandar para adicionar informações, depois para perfil
+}
+
+function adicionarInformações(){
+
+}
+
+function editarInformações(){
+
+}
 //DOMcontentLoaded + Eventos
+
+window.addEventListener('DOMContentLoaded', () => {
+    button.addEventListener('click', login);
+    button.addEventListener('click', adicionarInformações)
+});
